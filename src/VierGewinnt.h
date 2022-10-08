fb0bd93837e2ec63f12f9832decf8c0d032212ff
mortib20 <mortib20@gmail.com>
2022-10-08 16:21:24 +0200

    new version

--- src/VierGewinnt.h
+++ src/VierGewinnt.h
@@ -1,152 +1,190 @@
 #pragma once
-#ifdef linux
-#include <SDL2/SDL.h>
-#else
-#include <SDL.h>
-#endif
+#include"SDL.h"
 
-/* Rect Class */
-class Rect {
+/* SetField Set Enum with Player Empty  */
+
+class VierGewinnt
+{
 private:
-	SDL_Rect rect;
-public:
-	/* Constructor */
-	Rect(int X, int Y, int Width, int Height)
-	{
-		rect.x = X;
-		rect.y = Y;
-		rect.w = Width;
-		rect.h = Height;
+	static const int Width = 800;
+	static const int Height = 600;
+	static const int ColumnLength = 7;
+	static const int RowLength = 6;
+	static const int ColumnSize = Width / ColumnLength;
+	static const int RowSize = Height / RowLength;
+
+	int CurrentPlayer = 1;
+
+	int MouseX = 0;
+	int MouseY = 0;
+	int MouseDown = false;
+
+	int GameField[ColumnLength][RowLength] = { 0 };
+
+	SDL_Window* Window = SDL_CreateWindow("VierGewinnt", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, Width, Height + 1, 0);
+	SDL_Renderer* Renderer = SDL_CreateRenderer(Window, -1, SDL_RENDERER_PRESENTVSYNC);
+
+	int GetColumn() {
+		for (int c = 0; c < ColumnLength; c++)
+			if (MouseX >= ColumnSize * c && MouseX <= ColumnSize * (c + 1))
+				return c;
 	}
 
-	/* Return Rect */
-	SDL_Rect* GetRect()
-	{
-		return &rect;
-	}
-};
+	int GetRow() {
+		int Column = GetColumn();
 
-/* Window Class */
-class Window {
-protected:
-	const int Width;
-	const int Height;
-
-	SDL_Window* window;
-public:
-	/* Constructor */
-	Window(const char* Title, int Width, int Height, int Flags = 0) : Width(Width), Height(Height) 
-	{
-		window = SDL_CreateWindow(Title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, Width, Height, Flags);
-	};
-
-	/* Deconstructor */
-	~Window()
-	{
-		SDL_DestroyWindow(window);
-	};
-
-	/* Return Window */
-	SDL_Window* GetWindow()
-	{
-		return window;
-	};
-};
-
-/* Renderer Class */
-class Renderer {
-	SDL_Renderer* renderer;
-public:
-	/* De/Constructor */
-	Renderer(SDL_Window* window, uint32_t flags = 0)
-	{
-		renderer = SDL_CreateRenderer(window, -1, flags);
-	};
-
-	~Renderer()
-	{
-		SDL_DestroyRenderer(renderer);
+		for (int r = RowLength - 1; r > 0; r--)
+			if (GameField[Column][r] == 0)
+				return r;
 	}
 
-	/* Return Renderer */
-	SDL_Renderer* GetRenderer()
-	{
-		return renderer;
+	void SetField() {
+		int Column = GetColumn();
+		int Row = GetRow();
+
+		if (GameField[Column][Row] == 1 || GameField[Column][Row] == 2)
+			return;
+
+		if (CurrentPlayer == 1) {
+			GameField[Column][Row] = 1;
+			CurrentPlayer = 2;
+		}
+		else {
+			GameField[Column][Row] = 2;
+			CurrentPlayer = 1;
+		}
+		SDL_Log("[%i][%i] set", Column, Row);
 	}
 
-	/* Virtual */
-	virtual void Render() {
-		SetDrawColor(255, 0, 0);
-		DrawLine(0, 0, 10, 10);
+	void CheckLeftClick() {
+		uint32_t buttons;
+
+		SDL_PumpEvents();
+
+		buttons = SDL_GetMouseState(&MouseX, &MouseY);
+
+		if (!MouseDown && (buttons & SDL_BUTTON(1)) != 0) {
+			MouseDown = true;
+			SDL_Log("Mouse Click at x = %i y = %i", MouseX, MouseY);
+			SetField();
+			RenderFields();
+			RenderPresent();
+			CheckWinner(1);
+			CheckWinner(2);
+		}
+
+		if (buttons == 0)
+			MouseDown = false;
 	}
 
-	/* Color */
-	void SetDrawColor(uint8_t r, uint8_t g, uint8_t b)
+	void CheckWinner(int player)
 	{
-		SDL_SetRenderDrawColor(renderer, r, g, b, SDL_ALPHA_OPAQUE);
+		bool Winner = false;
+
+		// Row Check
+		for (auto c = 0; c < ColumnLength; c++)
+			for (int r = 0; r < RowLength; r++)
+				if (GameField[c][r] == player && GameField[c + 1][r] == player && GameField[c + 2][r] == player && GameField[c + 3][r] == player)
+					Winner = true;
+
+		// Column Check
+		for (auto c = 0; c < ColumnLength; c++)
+			for (int r = 0; r < RowLength; r++)
+				if (GameField[c][r] == player && GameField[c][r + 1] == player && GameField[c][r + 2] == player && GameField[c][r + 3] == player)
+					Winner = true;
+
+		// Right to Left Bottom Check
+		for (auto c = 0; c < ColumnLength; c++)
+			for (int r = 0; r < RowLength; r++)
+				if (GameField[c][r] == player && GameField[c + 1][r + 1] == player && GameField[c + 2][r + 2] == player && GameField[c + 3][r + 3] == player)
+					Winner = true;
+
+		// Left To Right Top Check
+		for (auto c = 0; c < ColumnLength; c++)
+			for (int r = 0; r < RowLength; r++)
+				if (GameField[c][r] == player && GameField[c - 1][r + 1] == player && GameField[c - 2][r + 2] == player && GameField[c - 3][r + 3] == player)
+					Winner = true;
+
+		if (Winner) {
+			if (player == 1)
+				SDL_ShowSimpleMessageBox(0, "Winner!", "Player 1 won the game!", Window);
+			if (player == 2)
+				SDL_ShowSimpleMessageBox(0, "Winner!", "Player 2 won the game!", Window);
+			EndGame();
+		}
 	}
 
-	/* Drawing */
-	void DrawLine(int x1, int y1, int x2, int y2)
-	{
-		SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
-	}
-
-	void DrawRect(Rect rect)
-	{
-		SDL_RenderDrawRect(renderer, rect.GetRect());
-	}
-
-	/* Present */
-	void Present()
-	{
-		SetDrawColor(0, 0, 0);
-		SDL_RenderClear(renderer);
-		Render();
-		SDL_RenderPresent(renderer);
-	}
-};
-
-class Events {
-public:
-	/* Events */
-	virtual void EventQuit()
-	{
-		SDL_Quit();
-	}
-
-	void PollEvent()
-	{
+	void CheckQuit() {
 		SDL_Event event;
-		while(SDL_PollEvent(&event))
-			if(event.type == SDL_QUIT)
-				EventQuit();
+
+		while (SDL_PollEvent(&event))
+			if (event.type == SDL_QUIT)
+				Running = false;
 	}
-};
 
-class GameTemplate : protected Window, protected Renderer, public Events {
-private:
+	void Input() {
+		CheckQuit();
+		CheckLeftClick();
+	}
 
+	void EndGame() {
+		Running = false;
+	}
+
+	void RenderFields() {
+		for (int c = 0; c < ColumnLength; c++) {
+			for (int r = 0; r < RowSize; r++) {
+				if (GameField[c][r] == 0)
+					DrawColor(0, 0, 0);
+
+				if (GameField[c][r] == 1)
+					DrawColor(0, 255, 0);
+
+				if (GameField[c][r] == 2)
+					DrawColor(0, 0, 255);
+
+				FillRect(RowSize - 1, ColumnSize - 1, (ColumnSize * c) + 1, (RowSize * r) + 1);
+			}
+		}
+	}
+
+	void RenderLines() {
+		DrawColor(255, 255, 255);
+
+		for (int c = 0; c <= ColumnLength; c++)
+			DrawLine(ColumnSize * c, 0, ColumnSize * c, Height);
+
+		for (int r = 0; r <= RowLength; r++)
+			DrawLine(0, RowSize * r, Width, RowSize * r);
+	}
+
+	void RenderPresent() {
+		SDL_RenderPresent(Renderer);
+	}
+
+	void DrawLine(int x1, int y1, int x2, int y2) {
+		SDL_RenderDrawLine(Renderer, x1, y1, x2, y2);
+	}
+
+	void FillRect(int h, int w, int x, int y) {
+		SDL_Rect Rect;
+		Rect.h = h;
+		Rect.w = w;
+		Rect.x = x;
+		Rect.y = y;
+
+		SDL_RenderFillRect(Renderer, &Rect);
+	}
+
+	void DrawColor(uint8_t r, uint8_t g, uint8_t b) {
+		SDL_SetRenderDrawColor(Renderer, r, g, b, SDL_ALPHA_OPAQUE);
+	}
 public:
-	/* Variables */
 	bool Running = true;
 
-	/* Constructor */
-	GameTemplate(const char* Title, int WinWidth, int WinHeight) : Window(Title, WinWidth, WinHeight), Renderer(GetWindow(), 0)
-	{
-		SDL_Init(SDL_INIT_EVERYTHING);
-	}
-
-	/* Deconstructor */
-	~GameTemplate()
-	{
-		SDL_Quit();
-	}
-
-	/* Run */
-	void Run()
-	{
-		PollEvent();
-		Present();
+	void Run() {
+		Input();
+		RenderLines();
+		RenderPresent();
 	}
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,8 @@
 #include "VierGewinnt.h"
 
-class VierGewinnt : virtual public GameTemplate
-{
-public:
-	//VierGewinnt(const char* Title, int WinWidth, int WinHeight) : GameTemplate(Title, WinWidth, WinHeight) {};
-	/* Constructor from GameTemplate maybe replace with other one */
-	using GameTemplate::GameTemplate;
-
-	/* What happens if SDL_QUIT */
-	void EventQuit()
-	{
-		Running = false;
-	}
-
-	void RenderLines()
-	{
-		SetDrawColor(255, 255, 255);
-
-		int rowSize = Height / 6;
-		int columnSize = Width / 7;
-
-		for (int i = 1; i < 6; i++)
-			DrawLine(0, (rowSize * i), Width, (rowSize * i));
-
-		for (int i = 1; i < 7; i++)
-			DrawLine((columnSize * i), 0, (columnSize * i), Height);
-	}
-
-
-	/* Place to Render */
-	void Render()
-	{
-		/*SetDrawColor(255, 0, 0);
-		DrawLine(0, 0, Width, Height);*/
-		RenderLines();
-	}
-};
-
 int main(int argc, char* argv[])
 {
-	VierGewinnt vierGewinnt("VierGewinnt", 800, 600);
+	VierGewinnt vierGewinnt;
 
 	while(vierGewinnt.Running)
 		vierGewinnt.Run();
